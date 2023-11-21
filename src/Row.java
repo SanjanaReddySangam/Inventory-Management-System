92f2211ec13e0367042d80cfb6fa8a1e67970351
Sanjana Reddy Sangam <ss8227@srmist.edu.in>
2023-11-21 19:48:39 +0530

    Add files via upload

---
+++ src/Row.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author acer
+ */
+public class Row {
+    private static int row;
+    public static int getRow(){
+        return row;
+    }
+    public static void setRow(){
+        row++;
+    }
+    public static void setRow( int temp){
+        row= temp;
+    }
+
+
+}
